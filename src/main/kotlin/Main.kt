4df295892cf8b764e1d84b628bc9aedbe2a16f64
Ezequiel Ramiro de Vasconcellos <erdevasconcellos@gmail.com>
2022-08-18 09:49:41 -0300

    readme

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,21 +9,16 @@ fun main() = application {
         title = "Navigation Compose (for Desktop and others)"
     ) {
 
-
         Navigator.run {
-
             addScreen(route = ScreensRoutes.MainScreen.route, isHome = true) {
                 MainScreen()
             }
-
             addScreen(route = ScreensRoutes.DetailScreen.route) {
                 DetailScreen()
             }
-
             renderUI()
-
         }
-        
+
     }
 }
 
